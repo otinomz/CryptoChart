5e49ebb1438bb1578ad7770b5e1e2b2665fef7c0
otinomz <awesomeotinomz@gmail.com>
2021-09-21 16:10:07 +0100

    last commit for currency page

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -7,11 +7,10 @@ import {useGetCryptosQuery} from "../services/CryptoApi"
 function CryptoCurrencies({simplified}) {
     const count = simplified ? 10: 100
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    const [cryptos, setCryptos] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
     
     useEffect(() => {
-        setCryptos(cryptosList?.data?.coins)
         const filteredData = cryptosList?.data?.coins.filter((coin)=> coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
     
         setCryptos(filteredData)
@@ -23,9 +22,13 @@ function CryptoCurrencies({simplified}) {
 
     return (
         <>
-            <div className="search-crypto">
-                <Input placeholder=" Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}/>
-            </div>
+            {!simplified && (
+                    <div className="search-crypto">
+                        <Input placeholder=" Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}/>
+                    </div>
+                )
+            }
+
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {
                     cryptos?.map((currency) => (
