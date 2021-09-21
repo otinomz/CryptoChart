1c8068f7e1c76b914fa6d1eac636bb867683fa94
otinomz <awesomeotinomz@gmail.com>
2021-09-21 12:03:02 +0100

    setting up the router for the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import {Navbar} from './components'
+
 
 function App() {
     return (
-        <div>
-            <h1>Crypto Wallet </h1>
+        <div className="app">
+            <div className="navbar">
+                <Navbar/>
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
         </div>
     )
 }
