3f066d384292aecd96bc7b3a3719da3e2453df60
otinomz <awesomeotinomz@gmail.com>
2021-09-21 13:20:19 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -34,10 +34,18 @@ function App() {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer">
 
-            </div>
+                <div className="footer" >
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        CryptoWall <br /> All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>       
         </div>
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+
+const {Title} = Typography
 
 function HomePage() {
     return (
-        <div>
-            home
-        </div>
+        <>
+            <Title level={2} className="heading"> Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value="10" /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="10" /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="10" /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value="10" /></Col>
+                <Col span={12}><Statistic title="Total Market" value="10" /></Col>
+            </Row>
+        </>
     )
 }
 
