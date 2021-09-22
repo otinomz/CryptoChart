7bec2ea231377175441b0f3e25c2ff9a0430c4b9
otinomz <awesomeotinomz@gmail.com>
2021-09-22 11:33:04 +0100

    changes to the news component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -34,10 +34,12 @@ function HomePage() {
                 <Title level={3} className="show-more"><Link to='cryptocurrencies'>Show more</Link></Title>
             </div>
             <CryptoCurrencies simplified/>
+            
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
                 <Title level={3} className="show-more"><Link to='news'>Show more</Link></Title>
             </div>
+
             <News simplified/>
         </>
     )
